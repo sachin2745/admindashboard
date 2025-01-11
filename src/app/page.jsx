6da72dfebc3a768b74843a78209f49a9908376fa
sachin2745 <sachin.official648@gmail.com>
2025-01-11 18:22:04 +0530

    Next JS and Tailwind and Preline Setup

---
+++ src/app/page.jsx
@@ -0,0 +1,21 @@
+"use client";
+import Link from 'next/link';
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <div className=" mx-auto p-24 text-center  border-2 border-sky-500 bg-sky-500 text-white  rounded-lg">
+        <h1 className="text-3xl font-bold">Welcome to TailAdmin Dashboard</h1>
+        <p className="mt-2 text-white">
+          Want to see click below on this <br />
+          <Link href="http://localhost:3000/dashboard" className="text-white font-bold">
+            Link            
+          </Link>
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default HomePage
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,6 +4,7 @@ export default {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './node_modules/preline/preline.js',
   ],
   theme: {
     extend: {
@@ -13,5 +14,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [ require('preline/plugin'),],
 };
